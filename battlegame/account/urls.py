51226254677b9449bd9dd7b5ebfac9750d2f6afe
Stefanos <stefanos6226@gmail.com>
2018-03-01 21:23:32 +0200

    creates base.html and register.html. adds app_name to account/urls.py

--- battlegame/account/urls.py
+++ battlegame/account/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "account"
+
 urlpatterns = [
     url(r"^register/$", views.register, name="register"),
     url(r"^login/$", views.user_login, name="user_login"),
