e96156b4189dace0d7b69bc7061ee5acc35295fc
Stefanos <stefanos@giaola.com>
2018-03-01 21:50:43 +0200

    adds LOGIN_ENABLED setting into settings.py

--- battlegame/account/views.py
+++ battlegame/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.conf import settings
 from .forms import UserRegistrationForm, UserLoginForm
 
 
