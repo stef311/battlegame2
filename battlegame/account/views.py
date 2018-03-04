42f5d871a0027b0b3880c3a5149c348bff0c83ad
Stefanos <stefanos6226@gmail.com>
2018-03-04 15:28:48 +0200

    adds logout functionality

--- battlegame/account/views.py
+++ battlegame/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -58,6 +58,12 @@ def user_login(request):
         user_form = UserLoginForm()
         context = {"user_form": user_form}
         return render(request, "account/login.html", context=context)
+
+@login_required
+def user_logout(request):
+    logout(request)
+    messages.add_message(request, messages.INFO, "You successfully logged out")
+    return redirect("account:user_login")
 
 @login_required
 def edit_profile(request):
