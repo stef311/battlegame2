1e8628320739a1a8e03b8938baf58d45ee71a719
Stefanos <stefanos@giaola.com>
2018-03-04 16:19:32 +0200

    adds central, army and market building main urls

--- battlegame/battle/urls.py
+++ battlegame/battle/urls.py
@@ -6,4 +6,8 @@ app_name = "battle"
 urlpatterns = [
     url(r"^dashboard/$", views.dashboard, name="dashboard"),
     url(r"^info/$", views.info, name="info"),
+    url(r"^central/$", views.info, name="central"),
+    url(r"^army/$", views.info, name="army"),
+    url(r"^market/$", views.info, name="market"),
+
 ]
