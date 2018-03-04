5bdfa7d1e5bf6997c14bcb7ba5b3d163adb913f0
Stefanos <stefanos@giaola.com>
2018-03-04 15:28:48 +0200

    adds logout functionality

--- battlegame/account/urls.py
+++ battlegame/account/urls.py
@@ -6,5 +6,6 @@ app_name = "account"
 urlpatterns = [
     url(r"^register/$", views.register, name="register"),
     url(r"^login/$", views.user_login, name="user_login"),
+    url(r"^logout/$", views.user_logout, name="user_logout"),
     url(r"^profile/$", views.edit_profile, name="edit_profile")
 ]
