aae0156dbf53aaaaf2fe44c355326ca15db6673f
Stefanos <stefanos6226@gmail.com>
2018-03-04 15:46:40 +0200

    adds info page. login is now required to view the dashboard.

--- battlegame/battle/views.py
+++ battlegame/battle/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+@login_required
 def dashboard(request):
     return HttpResponse("dashboard here")
+
+@login_required
+def info(request):
+    user = request.user
+    user_tribe = user.profile.tribe
+    context = {}
+    context["tribe"] = user_tribe
+    return render(request, "battle/info.html", context=context)
+
