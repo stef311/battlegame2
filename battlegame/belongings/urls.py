e135f9d5d62cdbd31b80a6d0b23ee725344f1747
Stefanos <stefanos@giaola.com>
2018-03-06 03:26:58 +0200

    create simple market, warehouse and resource_exchange views. adds corresponding urls to app url config

--- battlegame/belongings/urls.py
+++ battlegame/belongings/urls.py
@@ -5,4 +5,8 @@ app_name = "belongings"
 
 urlpatterns = [
     url(r"^overview/$", views.overview, name="overview"),
+    url(r"^market/$", views.market, name="market"),
+    url(r"^resource_exchange/$", views.resource_exchange, name="resource_exchange"),
+    url(r"^warehouse/$", views.warehouse, name="warehouse"),
+
 ]
--- battlegame/belongings/views.py
+++ battlegame/belongings/views.py
@@ -8,4 +8,18 @@ from django.contrib.auth.decorators import login_required
 def overview(request):
     return HttpResponse("overview of all items on all your villages")
 
+@login_required
+def market(request):
+    return HttpResponse("market building here. exchange resources. buy items")
+
+@login_required
+def resource_exchange(request):
+    return HttpResponse("user can redirect here from market and warehouse too. can exchange resources")
+
+@login_required
+def warehouse(request):
+    return HttpResponse("warehouse building here. exchange resources. here you can see all your resources and how much you earn per turn")
+
+
+
 
