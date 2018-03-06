750c4e0b2a63820a873816f2b0cef3f1e6728e67
Stefanos <stefanos@giaola.com>
2018-03-06 20:27:32 +0200

    renames UnitType model to Unit model. removes count from the model

--- battlegame/battle/models.py
+++ battlegame/battle/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.conf import settings
 
-class UnitType(models.Model):
+class Unit(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    count = models.IntegerField(default=0)
     name = models.CharField(max_length=20)
     attack_power = models.IntegerField(default=0)
     defense_power = models.IntegerField(default=0)
