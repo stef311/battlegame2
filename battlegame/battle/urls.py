a4037e9c457304286c2d97d1fa90c2f46da619af
Stefanos <stefanos@giaola.com>
2018-03-06 03:41:21 +0200

    renames 'info' to 'overview' in battle app views andurls

--- battlegame/battle/urls.py
+++ battlegame/battle/urls.py
@@ -5,5 +5,5 @@ app_name = "battle"
 
 urlpatterns = [
     url(r"^dashboard/$", views.dashboard, name="dashboard"),
-    url(r"^info/$", views.info, name="info"),
+    url(r"^overview/$", views.overview, name="overview"),
 ]
