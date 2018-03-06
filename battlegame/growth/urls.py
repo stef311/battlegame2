134c80c67f91a0edf8a45b72bc5a8afca295729d
Stefanos <stefanos6226@gmail.com>
2018-03-06 20:59:36 +0200

    adds LOGIN_URL in settings. upgrades view for museum(still simple)

--- battlegame/growth/urls.py
+++ battlegame/growth/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "battle"
 
 urlpatterns = [
-    url(r"^academy/$", views.academy, name="academy"),
-    url(r"^museum/$", views.museum, name="museum"),
+    url(r"^(?P<village_id>[0-9]+)/academy/$", views.academy, name="academy"),
+    url(r"^(?P<village_id>[0-9]+)/museum/$", views.museum, name="museum"),
 ]
