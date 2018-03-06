d384d5610e1ac96c84ecb41946d27753078121d7
Stefanos <stefanos6226@gmail.com>
2018-03-06 20:51:17 +0200

    create url configuration for simple growth app. creates simple views for museum and academy

--- battlegame/growth/views.py
+++ battlegame/growth/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def overview(request):
+    return HttpResponse("overview of all items on all your villages")
+
+@login_required
+def academy(request):
+    return HttpResponse("academy building here.")
+
+@login_required
+def museum(request):
+    return HttpResponse("museum building here")
+
+
+
