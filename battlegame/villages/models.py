a25673a2f7bd61354e72a22f5a16cb9d09616d6c
Stefanos <stefanos@giaola.com>
2018-03-06 03:11:59 +0200

    user in village model is ForeignKey field now instead of OneToOneField

--- battlegame/villages/models.py
+++ battlegame/villages/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 
 class Village(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     attack_power = models.IntegerField(default=0)
     defense_power = models.IntegerField(default=0)
@@ -12,4 +12,5 @@ class Village(models.Model):
     culture = models.IntegerField(default=0)
     science = models.IntegerField(default=0)
     description = models.CharField(max_length=2000)
+
 
