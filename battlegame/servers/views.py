754976efab409ad12a2061d4ded8ad7b4f114602
Stefanos <stefanos@giaola.com>
2018-03-08 19:46:00 +0200

    creates CreateGameServerForm and adds it in corresponding template

--- battlegame/servers/views.py
+++ battlegame/servers/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
+from .forms import CreateGameServerForm
+from .models import GameServer
+
 # Create your views here.
 
 def server_list(request):
@@ -8,4 +11,14 @@ def server_list(request):
 
 @staff_member_required
 def server_create(request):
-    return HttpResponse("Only staff can access this page")
+    if request.method == "GET":
+        create_gameserver_form = CreateGameServerForm()
+        context = {}
+        context.update({"create_gameserver_form": create_gameserver_form})
+        return render(request, "servers/create.html", context=context)
+    else:
+        create_gameserver_form = CreateGameServerForm(request.POST)
+        if create_gameserver_form.is_valid():
+            return HttpResponse("POST method used")
+        else:
+            return HttpResponse("not valid")
