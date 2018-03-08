cb752c92b9b67759ce805a9d2d2e949d3186e0c0
Stefanos <stefanos@giaola.com>
2018-03-08 19:29:34 +0200

    create GameServer model

--- battlegame/servers/models.py
+++ battlegame/servers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class GameServer(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    description = models.TextField(max_length=100)
+    players_allowed = models.IntegerField(default=0)
+    players_registered = models.IntegerField(default=0)
