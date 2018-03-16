0ee2c7e8b0121a2295fa5bacfe4e3bf76cf07d51
Stefanos <stefanos6226@gmail.com>
2018-03-17 01:02:45 +0200

    adds turn field in GameServer model, makes OneTimeTask and ContinuousTask abstract base classes for other models to be written

--- battlegame/servers/models.py
+++ battlegame/servers/models.py
@@ -6,17 +6,28 @@ from .appconfig import TASK_TYPES
 
 class GameServer(models.Model):
     name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    turn = models.IntegerField(default=0)
+    password = models.CharField(max_length=20)  
     description = models.TextField(max_length=20)
     players_allowed = models.IntegerField(default=0)
     players = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
+
 class OneTimeTask(models.Model):
     server = models.ForeignKey(GameServer, on_delete=models.CASCADE)
-    task_type = models.IntegerField(choices=TASK_TYPES,default=0)
-    players = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    
-    building = models.CharField(max_length=20)
+    turn = models.IntegerField(default=0)
+
+    class Meta:
+        abstract=True
 
 
-class DurationAction(models.Model):
+class ContinuousTask(models.Model):
+    server = models.ForeignKey(GameServer, on_delete=models.CASCADE)
+    turn_begin = models.IntegerField(default=0)
+    turn_end = models.IntegerField(default=0)
+
+    class Meta:
+        abstract=True
+
+
+
